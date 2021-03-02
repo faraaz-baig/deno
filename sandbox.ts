dbdc01bc12c9b8a46865441785b1570e38e49a2a
faraaz baig <fazzy0908@yandex.com>
2021-03-02 16:36:22 +0530

    Update sandbox.ts

--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,9 @@
-// fetch api in deno
+import { serve } from "https://deno.land/std@0.88.0/http/server.ts";
 
-const res = await fetch('https://swapi.dev/api/films/')
-const data = await res.json()
+const server = serve({ port: 3000 })
+console.log('listening to requests on port 3000')
 
-console.log(data)
+for await (const req of server) {
+    const url = req.url
+    req.respond({ body: `Hello Faraaz, you visited ${ url }` })
+}
